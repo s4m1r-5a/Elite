fd165083b763c55673c23a7d62c1cda29e831fd9
Samir Saldarriaga <59989740+s4m1r-5a@users.noreply.github.com>
2021-06-25 11:16:57 -0500

    Implementacion de los extratos bancarios

--- src/keys.js
+++ src/keys.js
@@ -56,7 +56,25 @@ module.exports = {
         redirect_uris: 'http://localhost:5000/contactos'
     }
 };
+/* Configurar Analytics Ads en tu sitio web
+Para conectar Google Analytics con tu sitio web, elige el método que corresponda de entre los siguientes:
+Opción 1: Añadir un ID de seguimiento a través de tu servicio de alojamiento o plataforma de comercio electrónico
+Inicia sesión en tu creador de sitios web, servicio de alojamiento web o plataforma de comercio electrónico y pega tu ID de seguimiento en la sección de analíticas.
+    UA - 200453767 - 1
 
+Opción 2: Añadir una etiqueta de seguimiento al código del sitio web
+Pega la etiqueta de seguimiento antes de la etiqueta de cierre </head > en cada una de las páginas del sitio web del que quieres hacer un seguimiento.
+
+< !--Global site tag(gtag.js) - Google Analytics-- >
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-200453767-1">
+</script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-200453767-1');
+</script> */
 
 //{"payment_method_type":"2","date":"2021.03.04 01:13:42","pse_reference3":"","pse_reference2":"","franchise":"MASTERCARD","commision_pol":"0.00","pse_reference1":"","shipping_city":"","bank_referenced_name":"","sign":"543b4f975ac347b85200b3b3c6f17b5c","extra2":"2056600~0~ABONO~424~0~0~0","extra3":"","operation_date":"2021-03-04 01:13:42","billing_address":"","payment_request_state":"A","extra1":"","administrative_fee":"0.00","administrative_fee_tax":"0.00","bank_id":"11","nickname_buyer":"","payment_method":"11","attempts":"1","transaction_id":"00274ba2-4c24-4f0c-93ef-13d4bc475ad4","transaction_date":"2021-03-04 01:13:42","test":"0","exchange_rate":"1.00","ip":"10.0.0.114","reference_pol":"1400036584","cc_holder":"APPROVED","tax":"0.00","antifraudMerchantId":"","pse_bank":"","state_pol":"4","billing_city":"","phone":"57 3007753983","error_message_bank":"0","shipping_country":"CO","error_code_bank":"00","cus":"664061","customer_number":"","description":"ABONO PRADOS DE PONTEVEDRA Mz 9 Lote: 15","merchant_id":"508029","administrative_fee_base":"0.00","authorization_code":"RBM353","currency":"COP","shipping_address":"","cc_number":"************3917","installments_number":"1","nickname_seller":"","value":"2056600.00","transaction_bank_id":"RBM353","billing_country":"CO","response_code_pol":"1","payment_method_name":"MASTERCARD","office_phone":"","email_buyer":"s4m1r.5a@gmail.com","payment_method_id":"2","response_message_pol":"APPROVED","account_id":"512321","airline_code":"","pseCycle":"","risk":"0.0","reference_sale":"1741-248-4JU","additional_value":"0.00"}
 
