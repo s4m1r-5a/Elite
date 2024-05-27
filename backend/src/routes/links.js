292361d6a0fad6c4ad7923451c37e2b08fdae8c0
Samir Saldarriaga <59989740+s4m1r-5a@users.noreply.github.com>
2024-05-27 13:58:12 -0500

    fdgs

--- backend/src/routes/links.js
+++ backend/src/routes/links.js
@@ -9026,7 +9026,7 @@ async function WspNewUser(empresa, movil, msg, options) {
     data: JSON.stringify({
       number: cel,
       message: msg,
-      urlMedia: options?.media.indexOf('localhost')
+      urlMedia: options?.media.indexOf('localhost') >= 0
         ? 'https://www.imprentaonline.net/blog/wp-content/webpc-passthru.php?src=https://www.imprentaonline.net/blog/wp-content/uploads/DALL%C2%B7E-2023-10-16-10.41.49-Illustration-depicting-a-humanoid-robot-with-half-of-its-face-transparent-revealing-intricate-circuits-and-gears-inside.-The-robot-is-holding-a-light-1.png&nocache=1'
         : options?.media || null
     })
